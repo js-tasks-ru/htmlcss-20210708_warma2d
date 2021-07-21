79cb380f99d9e46b081518f2b02f21fc1dcdc8a7
warma2d <warma2d@ya.ru>
2021-07-21 18:25:10 +0300

    fixed

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,21 +2,18 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
-    const darkLayer = document.getElementById('darkLayer');
 
-    if (!close || !modalOpen || !modal || !darkLayer) {
+    if (!close || !modalOpen || !modal) {
         return;
     }
 
     modalOpen.addEventListener('click', () => {
         modal.style.display = 'block';
         document.body.overflow = 'hidden';
-        darkLayer.style.display = 'block';
     });
 
     close.addEventListener('click', () => {
         modal.style.display = 'none';
         document.body.overflow = 'initial';
-        darkLayer.style.display = 'none';
     });
 })();
