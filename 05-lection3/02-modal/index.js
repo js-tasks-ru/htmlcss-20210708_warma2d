ff83962cb98582ac4ced6bc849645c25633a3dad
warma2d <warma2d@ya.ru>
2021-07-21 12:18:21 +0300

    added modal window

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,18 +2,21 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
+    const darkLayer = document.getElementById('darkLayer');
 
-    if (!close || !modalOpen || !modal) {
+    if (!close || !modalOpen || !modal || !darkLayer) {
         return;
     }
 
     modalOpen.addEventListener('click', () => {
         modal.style.display = 'block';
         document.body.overflow = 'hidden';
+        darkLayer.style.display = 'block';
     });
 
     close.addEventListener('click', () => {
         modal.style.display = 'none';
         document.body.overflow = 'initial';
+        darkLayer.style.display = 'none';
     });
 })();
