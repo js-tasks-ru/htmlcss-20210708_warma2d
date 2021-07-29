297497d05b1c541f7d1ac37dc8d80a0ce6f8afda
warma2d <warma2d@ya.ru>
2021-07-29 08:28:19 +0300

    fixed layout

--- 07-lection5/04-input/index.js
+++ 07-lection5/04-input/index.js
@@ -6,23 +6,18 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let key = 0; key < textBoxInputs.length; key++) {
 
         let input = textBoxInputs[key].children[0]
-        let placeholder = textBoxInputs[key].children[1]
-        let icon = textBoxInputs[key].children[2]
+        let icon = textBoxInputs[key].children[1]
 
         if (input.value !== "") {
-            placeholder.classList.add(HIDE)
             icon.classList.remove(HIDE)
         } else {
-            placeholder.classList.remove(HIDE)
             icon.classList.add(HIDE)
         }
 
         input.addEventListener('input', (event) => {
             if (event.target.value !== "") {
-                placeholder.classList.add(HIDE)
                 icon.classList.remove(HIDE)
             } else {
-                placeholder.classList.remove(HIDE)
                 icon.classList.add(HIDE)
             }
         });
